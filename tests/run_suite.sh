c50f77668f82c0385669a2fcf82badce376f809d
tobber <tobber@tobber-ThinkStation-P520>
2026-09-06 21:46:18 +0200

    test: add tests 85-87 for new Ghost weapons
    - Test 85: Ghost beam weapon
    - Test 86: Ghost cluster rocket
    - Test 87: Ghost EMP wave
    - Chunk 4: 73-87 (was 73-84)

--- tests/run_suite.sh
+++ tests/run_suite.sh
@@ -945,6 +945,30 @@ assert_84() {
   report $ok "boss-demolisher"
 }
 
+# TEST 85: Ghost beam weapon
+assert_85() {
+  local ok=0 logfile="$1"
+  check "$logfile" "BEAM"; [ $LAST_RESULT -eq 0 ] || ok=1
+  no_fatal_warnings "$logfile" || ok=1
+  report $ok "ghost-beam"
+}
+
+# TEST 86: Ghost cluster rocket
+assert_86() {
+  local ok=0 logfile="$1"
+  check "$logfile" "CLUSTER ROCKET"; [ $LAST_RESULT -eq 0 ] || ok=1
+  no_fatal_warnings "$logfile" || ok=1
+  report $ok "ghost-cluster"
+}
+
+# TEST 87: Ghost EMP wave
+assert_87() {
+  local ok=0 logfile="$1"
+  check "$logfile" "EMP WAVE"; [ $LAST_RESULT -eq 0 ] || ok=1
+  no_fatal_warnings "$logfile" || ok=1
+  report $ok "ghost-empwave"
+}
+
 # TEST 2: full run victory
 assert_2() {
   local ok=0
@@ -1131,11 +1155,14 @@ dispatch_test() {
     82) run_test 82 "boss-shielder" 90 +set g_neonwave_autostart 1 +set g_neonwave_startwave 18 +set g_neonwave_bosstype 9 +set g_neonwave_fastbreak 1 +set g_neonwave_autokill 1 ;;
     83) run_test 83 "boss-snipelite" 90 +set g_neonwave_autostart 1 +set g_neonwave_startwave 19 +set g_neonwave_bosstype 10 +set g_neonwave_fastbreak 1 +set g_neonwave_autokill 1 ;;
     84) run_test 84 "boss-demolisher" 90 +set g_neonwave_autostart 1 +set g_neonwave_startwave 20 +set g_neonwave_bosstype 11 +set g_neonwave_fastbreak 1 +set g_neonwave_autokill 1 ;;
+    85) run_test 85 "ghost-beam" 60 +set g_neonwave_ghost 1 +set g_neonwave_autostart 1 +set g_neonwave_failrun 1 ;;
+    86) run_test 86 "ghost-cluster" 60 +set g_neonwave_ghost 1 +set g_neonwave_autostart 1 +set g_neonwave_failrun 1 ;;
+    87) run_test 87 "ghost-empwave" 60 +set g_neonwave_ghost 1 +set g_neonwave_autostart 1 +set g_neonwave_failrun 1 ;;
 
     *)  echo "no cvar mapping for test $1"; return 2 ;;
   esac
 }
-ALL_TESTS="1 2 3 4 5 6 7 8 9 9b 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84"
+ALL_TESTS="1 2 3 4 5 6 7 8 9 9b 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87"
 QUICK_TESTS="1 3 4 7 8 10 12 13 17 18 19 20 21 22 23 26 27 28 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81"
 
   case "$MODE" in
